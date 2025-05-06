344405a79986854426b9392a4e804fc89763b6d6
Hoangdev17 <hoangluudev17@gmail.com>
2025-05-06 16:49:33 +0700

    config env

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+dotenv.config();
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () =>{
   console.log(`Server running on ${PORT}`)
